9ee1a398c5152fc008efbb3451786bba01b74830
Takamasa-Fukase <ultrafukase@gmail.com>
2024-12-03 22:09:34 +0900

    minimum deployment targetを指定。また、ソースコードにUIKit使用可否の分岐を追加

---
+++ Sources/Sample_WebView_SDK/WebViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WebViewController.swift
+//
+//
+//  Created by ウルトラ深瀬 on 3/12/24.
+//
+
+#if canImport(UIKit)
+
+import UIKit
+import WebKit
+
+final class WebViewController: UIViewController {
+    
+}
+
+#endif
